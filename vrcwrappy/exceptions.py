ff059ee8f31b4ad8da30005657c75df35b5bd253
Katistic <katistix@gmail.com>
2023-02-13 22:52:00 +1100

    InvalidEvent Exception
    Exception that gets raised when trying to decorate a non-existent event

--- vrcwrappy/exceptions.py
+++ vrcwrappy/exceptions.py
@@ -7,3 +7,7 @@ class WebsocketEventError(Exception):
 class NotVRChatURLException(ApiException):
     def __init__(self, status=None, reason=None, http_resp=None):
         super().__init__(status, reason, http_resp)
+        
+class InvalidEvent(ApiException):
+    def __init__(self, event_name):
+        super().__init__("Event does not exist: '%s'" % event_name)
