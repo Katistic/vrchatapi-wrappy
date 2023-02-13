771284af690cf614a51d98a9504f126987677442
Katistic <katistix@gmail.com>
2023-02-13 22:50:54 +1100

    WebSocketEventError Exception Class
    Base class for websocket event errors

--- vrcwrappy/exceptions.py
+++ vrcwrappy/exceptions.py
@@ -1,5 +1,9 @@
 from vrchatapi.exceptions import ApiException
 
+class WebsocketEventError(Exception):
+    """Base Websocket Event Error"""
+    pass
+
 class NotVRChatURLException(ApiException):
     def __init__(self, status=None, reason=None, http_resp=None):
         super().__init__(status, reason, http_resp)
